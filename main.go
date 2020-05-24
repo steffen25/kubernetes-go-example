a9c5049abd24178b55bc80ad063dc8d4b542d750
Steffen Thomsen <acbolic@gmail.com>
2020-05-24 11:56:12 +0200

    Initial terraform cluster setup

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		for name, values := range c.Request.Header {
+			// Print headers
+			for _, value := range values {
+				log.Println(name, value)
+			}
+		}
+		c.String(http.StatusOK, "pong")
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
